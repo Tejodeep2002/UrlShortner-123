8bf54304c15405997ebc75fe09ea04aba786ea54
Tejodeep <tejodeepmitraroy2002@gmail.com>
2022-08-15 23:54:43 +0530

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import './Main File/UrlShortner.css'
 import Textarea from './Main File/UrlShortner';
 import 'bootstrap/dist/css/bootstrap.css';
 import Background from './Main File/Background';
-import Linkresult from './Main File/LinkResult';   
+import Linkresult from './Main File/LinkResult'; 
+import Footer from './Main File/Footer';  
 import { useState } from 'react';
+
 
 function App() {
   const [inputValue,setInputValue]=useState("");
@@ -12,6 +15,7 @@ function App() {
       <Textarea setInputValue={setInputValue}/>
       <Background/>
       <Linkresult inputValue={inputValue}/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Main File/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className='footer'>
+                <p>Created by Tejodeep Mitra Roy</p>
+            </div>
+          );   
+    }
+}
+ 
+export default Footer;
